429f10593216d0768d7d11406b0f676421002ab8
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-12-10 16:08:36 -0500

    added function to call a specific query as well as first query's callback

--- bamazonManager.js
+++ bamazonManager.js
@@ -6,7 +6,7 @@ var mainMenuPrompt = [
         type: 'list',
         message: 'Please select an option: ',
         name: 'mainMenuOption',
-        choices: ['View Products for Sale','View Low Inventory','Add to Inventory','Add New Product']
+        choices: ['View Products for Sale','View Low Inventory','Add to Inventory','Add New Product', 'Exit']
     }
 ]
 
@@ -14,7 +14,7 @@ function mainMenu() {
     prompt.prompt(mainMenuPrompt)
         .then(function(answers){
             if (answers.mainMenuOption == 'View Products for Sale') {
-                console.log('load products for sale fn')
+                queryRequested(productsQStr,null,productsForSale)
             }
             else if (answers.mainMenuOption == 'View Low Inventory' ) {
                 console.log('Low Invetory fn')
@@ -26,11 +26,45 @@ function mainMenu() {
                 console.log('add new product fn')
             }
             else {
-                console.log('something went wrong')
+                return 
             }
         })
     
 }
 
+// create connection pool
+var pool = mysql.createPool({
+    connectionLimit: 10, 
+    host: 'localhost', 
+    user: 'root',
+    password: 'password',
+    database: 'bamazon'
+})
+
+// relevant query strings 
+var productsQStr = `select * from products`
+
+// function that takes in a query string and a call back to do something
+function queryRequested(queryString, queryParams, callBack) {
+    pool.getConnection(function(err,connection) {
+        if (err) {
+            console.log(err)
+        }
+        connection.query(queryString,callBack)
+        connection.release()
+    })
+}
+
+// call back for products for sale 
+function productsForSale(err,results) {
+    if (err) {
+        console.log(err)
+    }
+    console.table(results)
+    mainMenu()
+}
+
 // call main menu 
 mainMenu()
+
+
