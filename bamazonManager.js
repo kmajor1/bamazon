9b637eccd2832216de9805019f405a03216a74c5
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-12-10 21:28:10 -0500

    inventory add functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,37 +1,6 @@
 const prompt = require('inquirer')
 const mysql = require('mysql')
 
-var mainMenuPrompt = [
-    {
-        type: 'list',
-        message: 'Please select an option: ',
-        name: 'mainMenuOption',
-        choices: ['View Products for Sale','View Low Inventory','Add to Inventory','Add New Product', 'Exit']
-    }
-]
-
-function mainMenu() {
-    prompt.prompt(mainMenuPrompt)
-        .then(function(answers){
-            if (answers.mainMenuOption == 'View Products for Sale') {
-                queryRequested(productsQStr,null,productsForSale)
-            }
-            else if (answers.mainMenuOption == 'View Low Inventory' ) {
-                queryRequested(lowInventoryQStr,lowInvParam,lowInventory)
-            }
-            else if (answers.mainMenuOption == 'Add to Inventory') {
-                console.log('add to inventory fn')
-            }
-            else if (answers.mainMenuOption == 'Add New Product') {
-                console.log('add new product fn')
-            }
-            else {
-                pool.end()
-            }
-        })
-    
-}
-
 // create connection pool
 var pool = mysql.createPool({
     connectionLimit: 10, 
@@ -45,6 +14,85 @@ var pool = mysql.createPool({
 var productsQStr = `select * from products`
 var lowInventoryQStr = `select * from products where stock_quantity < ?`
 var lowInvParam = ['5']
+var getProductCurQuantity = `select stock_quantity from products where id = ? `
+var addInvetoryQStr = `update products set stock_quantity = ? where id = ?`
+
+
+
+var mainMenuPrompt = [
+    {
+        type: 'list',
+        message: 'Please select an option: ',
+        name: 'mainMenuOption',
+        choices: ['View Products for Sale','View Low Inventory','Add to Inventory','Add New Product', 'Exit']
+    }
+]
+
+// main program loop 
+function mainMenu() {
+    prompt.prompt(mainMenuPrompt)
+        .then(function(answers){
+            if (answers.mainMenuOption == 'View Products for Sale') {
+                queryRequested(productsQStr,null,productsForSale)
+            }
+            else if (answers.mainMenuOption == 'View Low Inventory' ) {
+                queryRequested(lowInventoryQStr,lowInvParam,lowInventory)
+            }
+            else if (answers.mainMenuOption == 'Add to Inventory') {
+                
+                prompt.prompt([
+                    {
+                        type: 'input',
+                        name: 'productIdAdd',
+                        message: 'Please provide ID of product quantity to add'
+                    },
+                    {
+                        type: 'input',
+                        name: 'productIdAmtToAdd',
+                        message: 'Please enter quantity of product to add'
+                    }
+                ])
+                .then(function(answers){
+                    // get current quantity of product 
+                    var getStockQuantityParams = [answers.productIdAdd]
+                    pool.getConnection(function(err,connection) {
+                        if (err) {
+                            console.log(err)
+                        }
+                        connection.query(getProductCurQuantity, getStockQuantityParams, function(err, results){
+                            if (err) {
+                                console.log(err)
+                            }
+                            var stock_quantity = results[0].stock_quantity
+                            var quantToAdd = parseInt(answers.productIdAmtToAdd)
+                            var newTotal = stock_quantity + quantToAdd
+                            
+                            getStockQuantityParams = [newTotal,answers.productIdAdd]
+                            // call update query 
+                            connection.query(addInvetoryQStr, getStockQuantityParams, updateCurrentQuantity)
+                            connection.release()
+                            
+
+                        })
+                    })
+                    
+
+                })
+                
+            }
+            else if (answers.mainMenuOption == 'Add New Product') {
+                console.log('add new product fn')
+            }
+            else {
+                pool.end()
+            }
+        })
+    
+}
+
+
+
+
 
 // function that takes in a query string and a call back to do something
 function queryRequested(queryString, queryParams, callBack) {
@@ -70,7 +118,7 @@ function productsForSale(err,results) {
         console.log(err)
     }
     console.table(results)
-    mainMenu()
+    // mainMenu()
 }
 
 // callback for low inventory query 
@@ -79,6 +127,15 @@ function lowInventory(err, results) {
         console.log(err)
     }
     console.table(results)
+    mainMenu()
+}
+
+// Callback for get current quanity query 
+function updateCurrentQuantity(err, results){
+    if (err) {
+        console.log(err)
+    }
+    console.log('Updated!')
     mainMenu()
 }
 
